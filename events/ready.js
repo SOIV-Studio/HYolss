843650ecdea112b60244303de995da2c2112a77e
SOIV <cho020403@gmail.com>
2025-02-13 19:13:30 +0900

    feat: 봇 상태 메시지 추가

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,12 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType } = require('discord.js');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		
+		// 봇의 상태 설정
+		client.user.setActivity('지옥에서 살아 돌아왔다!', { type: ActivityType.Playing });
 	},
 };
