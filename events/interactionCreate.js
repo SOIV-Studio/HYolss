7dd37d03bf07f96c122c9fea29473819e9e9a175
SOIV <cho020403@gmail.com>
2025-02-13 23:47:02 +0900

    refactor: 메뉴추가 명령어 권한 체크 로직 개선

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,4 +1,4 @@
-const { Events, MessageFlags } = require('discord.js');
+const { Events, MessageFlags, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	name: Events.InteractionCreate,
@@ -13,6 +13,25 @@ module.exports = {
 		}
 
 		try {
+			// 메뉴추가 명령어에 대한 권한 체크
+			if (interaction.commandName === '메뉴추가') {
+				const allowedUserId = '336746851971891203';
+				if (interaction.user.id !== allowedUserId) {
+					const noPermissionEmbed = new EmbedBuilder()
+						.setColor('#FF0000')
+						.setTitle('❌ 권한 없음')
+						.setDescription('이 명령어를 사용할 권한이 없습니다.')
+						.setTimestamp()
+						.setFooter({ text: 'HYolss Bot' });
+
+					await interaction.reply({
+						embeds: [noPermissionEmbed],
+						ephemeral: true
+					});
+					return;
+				}
+			}
+
 			await command.execute(interaction);
 		} catch (error) {
 			console.error(error);
