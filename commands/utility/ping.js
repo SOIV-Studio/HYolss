ae91d5f68add6cffb18ba46c34fa791848b4f2df
SOIV <cho020403@gmail.com>
2025-02-15 19:10:18 +0900

    delete commands and update ping.js

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -3,8 +3,9 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setDescription('서버와의 핑 테스트를 보여줍니다.'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		const ping = interaction.client.ws.ping;
+		await interaction.reply(`Pong! (${ping}ms)`);
 	},
 };
